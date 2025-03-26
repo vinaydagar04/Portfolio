6b9947da3c4dd4a08d4bfde66a219cbf69c637fc
Vinay Kumar <vinaydagar514@gmail.com>
2025-03-27 01:19:38 +0530

    fix: update personal introduction in homepage

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -10,16 +10,15 @@ export default function Home() {
   return (
     <Container>
       <span className="text-4xl">👋</span>
-      <Heading className="font-black">Hello there! I&apos;m John</Heading>
+      <Heading className="font-black">Hello there! I&apos;m Vinay</Heading>
       <Paragraph className="max-w-xl mt-4">
         I&apos;m a full-stack developer that loves{" "}
         <Highlight>building products</Highlight> and web apps that can impact
         millions of lives
       </Paragraph>
       <Paragraph className="max-w-xl mt-4">
-        I&apos;m a senior software engineer with{" "}
-        <Highlight>7 years of experience</Highlight> building scalable web apps
-        that are performance optimized and good looking.
+        I have been building scalable web apps that are performance optimised
+        and stunning design for <Highlight>more than 1 years now</Highlight>
       </Paragraph>
       <Heading
         as="h2"
