5c26b48933a0726aadb4047cb9037bcb2ff28b0f
Vinay Kumar <vinaydagar514@gmail.com>
2025-03-27 00:58:50 +0530

    fix: update introduction name in homepage

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -10,7 +10,7 @@ export default function Home() {
   return (
     <Container>
       <span className="text-4xl">👋</span>
-      <Heading className="font-black">Hello there! I&apos;m John</Heading>
+      <Heading className="font-black">Hello there! I&apos;m Vinay</Heading>
       <Paragraph className="max-w-xl mt-4">
         I&apos;m a full-stack developer that loves{" "}
         <Highlight>building products</Highlight> and web apps that can impact
