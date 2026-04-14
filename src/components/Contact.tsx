0229f55df7c39eff6d568ab0f1f1b3b3794802eb
Vinay Kumar <vinaydagar514@gmail.com>
2026-04-14 16:58:44 +0530

    fix: update input and textarea styles in Contact component for consistency

--- src/components/Contact.tsx
+++ src/components/Contact.tsx
@@ -29,7 +29,7 @@ export const Contact = () => {
         <input
           type="text"
           placeholder="Your Name"
-          className="bg-gray-900 focus:outline-none focus:ring-2 focus:ring-neutral-100 px-2 py-2 rounded-md text-sm text-neutral-700 w-full"
+          className="bg-gray-900 focus:outline-none focus:ring-2 focus:ring-neutral-100 px-2 py-2 rounded-md text-sm text-neutral-200 w-full"
           value={formData.name.value}
           onChange={(e) => {
             setFormData({
@@ -44,7 +44,7 @@ export const Contact = () => {
         <input
           type="email"
           placeholder="Your email address"
-          className="bg-gray-900 focus:outline-none focus:ring-2 focus:ring-neutral-200 px-2 py-2 rounded-md text-sm text-neutral-700 w-full"
+          className="bg-gray-900 focus:outline-none focus:ring-2 focus:ring-neutral-200 px-2 py-2 rounded-md text-sm text-neutral-200 w-full"
           value={formData.email.value}
           onChange={(e) => {
             setFormData({
@@ -61,7 +61,7 @@ export const Contact = () => {
         <textarea
           placeholder="Your Message"
           rows={10}
-          className="bg-gray-900 focus:outline-none focus:ring-2 focus:ring-neutral-200 px-2 mt-4 py-2 rounded-md text-sm text-neutral-700 w-full"
+          className="bg-gray-900 focus:outline-none focus:ring-2 focus:ring-neutral-200 px-2 mt-4 py-2 rounded-md text-sm text-neutral-200 w-full"
           value={formData.message.value}
           onChange={(e) => {
             setFormData({
